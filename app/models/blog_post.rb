8c9e48c0e714914f6cfc70369b9663bef38e9b86
Jennifer Krohn <jenniferlynn1207@msn.com>
2015-03-05 11:37:30 -0500

    stylized show pg

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -2,7 +2,7 @@ class BlogPost < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments
 
-	has_attached_file :image, :styles => { :small => "200x200>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => /\Aimage\/.*\Z/
+	has_attached_file :image, :styles => { :small => "150x150>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => /\Aimage\/.*\Z/
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\z/
 
